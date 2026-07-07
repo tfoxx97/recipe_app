17f3b81b2ad3d3e7ac424817d6db13d2f472026f
= <=>
2026-07-07 14:12:13 -0700

    Initial commit to branch. Added a folder that contains self-signed SSL certificates. Updated run method of app to include the self-signed certificates.

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
+import os
 from recipe_app import app
 
+cert_path = os.path.abspath("recipe_app/certificates/cert.pem")
+key_path = os.path.abspath("recipe_app/certificates/key.pem")
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(ssl_context=(cert_path, key_path), host='0.0.0.0', debug=True)
